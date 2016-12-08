e950f3da7ab244ef5cc8c430b8c3097798d24cca
Veronika <nika_pisarenko@mail.ru>
2016-12-09 00:40:44 +0300

    Creating bot

---
+++ ClassLibraryBot/ReadingFromTimePad.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryBot
+{
+    
+        public class PosterImage
+        {
+            public string default_url { get; set; }
+            public string uploadcare_url { get; set; }
+        }
+
+        public class Category
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+        }
+
+        public class Value
+        {
+            public int id { get; set; }
+            public string starts_at { get; set; }
+            public string name { get; set; }
+            public string url { get; set; }
+            public PosterImage poster_image { get; set; }
+            public List<Category> categories { get; set; }
+            public string moderation_status { get; set; }
+        }
+
+        public class RootObject
+        {
+            public int total { get; set; }
+            public List<Value> values { get; set; }
+        }
+    
+}
--- LeisureManagerBot/Program.cs
+++ LeisureManagerBot/Program.cs
@@ -1,20 +1,47 @@
-﻿using System;
+﻿using ClassLibraryBot;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot.Types.ReplyMarkups;
 
-namespace LeisureManagerBot
+namespace Telegram.Bot
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("blablabla");
-            Console.WriteLine("blablabla");
-            Console.WriteLine("54543");
-            Console.WriteLine("54543");
-            Console.ReadLine();
+            Run().Wait();
         }
+        static async Task Run()
+        {
+            var Bot = new TelegramBotClient("306835183:AAHQLsRrPzOeDfHhQvlOOYi0X5kRJke7ngg");
+            try
+            {
+                var bot = await Bot.GetMeAsync();
+                Console.WriteLine("Hello my name is {0}", bot.Username);
+            }
+            catch { };
+            var rkm = new ReplyKeyboardMarkup();
+
+            string city = "Москва";
+            string url = "https://api.timepad.ru/v1/events.json?limit=25&skip=0&cities=" + city;
+            string str;
+
+            using (var webClient = new WebClient())
+            {
+
+                str = webClient.DownloadString(url);
+            }
+
+            byte[] bytes = Encoding.Default.GetBytes(str);
+            str = Encoding.UTF8.GetString(bytes);
+
+            RootObject account = JsonConvert.DeserializeObject<RootObject>(str);
+        }
+
     }
 }
