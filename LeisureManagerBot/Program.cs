7be7ed850949144f76dc7a50796d8278ced90482
Veronika <nika_pisarenko@mail.ru>
2016-12-08 23:50:33 +0300

    Start

---
+++ LeisureManagerBot/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeisureManagerBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("blablabla");
+            Console.WriteLine("54543");
+            Console.ReadLine();
+        }
+    }
+}
